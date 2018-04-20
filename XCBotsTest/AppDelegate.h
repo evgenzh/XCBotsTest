1dfbedde41aa0c622dedc71a0771bf07cb311d74
Eugene Zhuk <evgen.zhuk@together.com>
2018-04-20 15:56:40 +0300

    init

---
+++ XCBotsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCBotsTest
+//
+//  Created by Eugene Zhuk on 4/20/18.
+//  Copyright © 2018 Eugene Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCBotsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCBotsTest
+//
+//  Created by Eugene Zhuk on 4/20/18.
+//  Copyright © 2018 Eugene Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
